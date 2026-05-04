0a3d5f483351e22f32a610dbcc6eeee071df1fc8
SzaboPatrik <szabopatrikmain@gmail.com>
2026-05-04 09:37:43 +0200

    elkezdett haziko

--- KozosRajz.py
+++ KozosRajz.py
@@ -2,7 +2,15 @@ import turtle as t
 t.speed(0)
 t.color("green")
 t.width(3)
+
 i=0
+t.left(90)
+while i=4:
+    t.forward(100)
+    t.left(90)
+    i+=1
+    t.done()
+#Patrik,ket emeletes haz alja
 
 
 t.setheading(135)
