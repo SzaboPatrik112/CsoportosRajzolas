c7ea2cf066ca7c9e3ebee2091f487ef2bcb08003
SzaboPatrik <szabopatrikmain@gmail.com>
2026-05-11 08:28:18 +0200

    haziko alapja keszen, tetovel

--- KozosRajz.py
+++ KozosRajz.py
@@ -5,16 +5,20 @@ t.width(3)
 
 i=0
 t.left(90)
-while i=4:
+while i<4:
     t.forward(100)
     t.left(90)
     i+=1
-    t.done()
-#Patrik,ket emeletes haz alja
+t.forward(100)
+i=0
+while i<4:
+    t.forward(100)
+    t.left(90)
+    i+=1
 
-#Sebi hazteto
+t.forward(100)
 t.setheading(135)
-t.forward(60)
+t.forward(70)
 t.setheading(-135)
-t.forward(60)
+t.forward(70)
 t.done()
