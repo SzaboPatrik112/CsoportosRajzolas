3a421933e64c257f51371fdd86944759ef658a11
SchrankSebestyen <schranksebestyen@gmail.com>
2026-05-11 08:46:20 +0200

    Sebi teljesen kesz

--- KozosRajz.py
+++ KozosRajz.py
@@ -17,8 +17,36 @@ while i<4:
     i+=1
 
 t.forward(100)
-t.setheading(135)
+t.setheading(136)
 t.forward(70)
-t.setheading(-135)
+t.setheading(-136)
 t.forward(70)
+t.penup()
+t.setheading(270)
+t.forward(100)
+t.left(180)
+t.forward(30)
+t.right(90)
+t.forward(30)
+i=0
+t.pendown()
+while i<4:
+    t.forward(40)
+    t.left(90)
+    i+=1
+t.forward(20)
+t.left(90)
+t.forward(40)
+t.left(90)
+t.forward(20)
+t.left(90)
+t.forward(20)
+t.left(90)
+t.forward(40)
+t.penup()
+t.left(180)
+t.forward(70)
+t.left(90)
+t.forward(150)
+
 t.done()
